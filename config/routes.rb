179e96e2e4256ee5954524501ae1a6509f65611a
Jamie DeLuca <jamie_deluca@keybank.com>
2014-11-25 20:05:02 -0500

    Added Sign in and sign out features

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+	resource :session
 	get "signup" => "users#new"
+	get "sign-in" => "sessions#new"
   resources :users
+
   root "movies#index"
   resources :movies do
     resources :reviews
